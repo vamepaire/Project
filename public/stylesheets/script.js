e4169efe7358e49258a075825b7cf80d0ff0aa5d
Arghya Paul <arghyap2003@gmail.com>
2024-10-25 20:44:46 +0530

    Delete public directory

--- public/stylesheets/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const successMessage = document.getElementById("successMessage");
-  if (successMessage) {
-    setTimeout(() => {
-      successMessage.style.display = "none";
-    }, 2500);
-  }
-});
